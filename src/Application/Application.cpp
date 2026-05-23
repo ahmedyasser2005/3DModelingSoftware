7dc11c0aece1054864bc509cc9b4e8d79324a01c
Seef <139868951+Seef25@users.noreply.github.com>
2026-05-23 04:50:05 +0300

    Implement Line/Circle/Ellipse drawing algorithms (#3)
    * Added Line and circle algorithms inside Rasterizer header
    
    * Updated clang format for a weird issue that happened
    
    * Deleted Algorithms folder with the content inside
    
    * Added keybinds for drawing algorithms
    
    * Added LineBresenham, CircleCartesian, CirclePolar, CircleBresenham, LineDDA, MidpointCircle, ElipseCartesian, EllipsePolar, EllipseMidpoint
    
    * Added all algorithms mentioned in Draw.h
    
    * Added: LineFractionalAccumlation and EllipseFractionalAccumlation
    
    * Updated header to include LineFractionalAccumulation and EllipseFractionalAccumulation
    
    * Fixed typo
    
    * Added Draw

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,5 +1,5 @@
 #include "Application.h"
-
+#include "Draw/Draw.h"
 Application::Application() : m_Window({ L"3D Modeling Software Engine", 1280, 720 })
 {
     m_SoftwareRenderer.Initialize(m_Window.GetWidth(), m_Window.GetHeight());
@@ -74,6 +74,42 @@ void Application::Update(float deltaTime)
         m_SoftwareRenderer.PutPixel(mx + 1, my, 255, 200, 0);
         m_SoftwareRenderer.PutPixel(mx, my + 1, 255, 200, 0);
         m_SoftwareRenderer.PutPixel(mx + 1, my + 1, 255, 200, 0);
+    }
+    if (input.IsKeyPressed(KeyCode::K))
+    {
+        Draw::LineBresenham(&m_SoftwareRenderer, 100, 100, 400, 300, 0xFF00FF00);
+    }
+    if (input.IsKeyPressed(KeyCode::D)) 
+            {
+        Draw::LineDDA(&m_SoftwareRenderer, 200, 200, 500, 500, 0xFF0000FF);
+            }
+    if (input.IsKeyPressed(KeyCode::S))
+         {
+         Draw::CircleCartesian(&m_SoftwareRenderer, 400, 300, 100, 0xFFFF0000);
+         }
+    if (input.IsKeyPressed(KeyCode::C))
+    {
+        Draw::CirclePolar(&m_SoftwareRenderer, 600, 300, 80, 0xFFFFFF00);
+    }
+    if (input.IsKeyPressed(KeyCode::B))
+     {
+         Draw::CircleBresenham(&m_SoftwareRenderer, 800, 300, 60, 0xFFFF00FF);
+     }
+    if (input.IsKeyPressed(KeyCode::M))
+    {
+        Draw::CircleMidpoint(&m_SoftwareRenderer, 1000, 300, 120, 0xFF00FFFF);
+    }
+    if (input.IsKeyPressed(KeyCode::I))
+    {
+        Draw::EllipseCartesian(&m_SoftwareRenderer, 300, 500, 120, 70, 0xFF00FF00);
+    }
+    if (input.IsKeyPressed(KeyCode::O))
+    {
+        Draw::EllipsePolar(&m_SoftwareRenderer, 640, 500, 120, 70, 0xFFFFFF00);
+    }
+    if (input.IsKeyPressed(KeyCode::P))
+    {
+        Draw::EllipseMidpoint(&m_SoftwareRenderer, 980, 500, 120, 70, 0xFF00FFFF);
     }
 }
 
